f041fef8fa13265625cf4276ca9118e294df235d
lauriekell <laurie@kell.es>
2018-11-22 10:16:28 +0000

    tidy

--- tasks/task5/R/mse-emp-d-grid.R
+++ tasks/task5/R/mse-emp-d-grid.R
@@ -22,6 +22,11 @@ dirMy="/home/laurence/Desktop/Dropbox/mydasOMs"
 dirDat=file.path(dirMy,"data")
 dirRes=file.path(dirMy,"results")
 
+source('~/Desktop/sea++/mydas/pkg/R/mseSBTD.R')
+source('~/Desktop/sea++/mydas/pkg/R/hcrSBTD.R')
+
+mseStart=c("brill"=54,"turbot"=54,"ray"=60,"pollack"=56,"sprat"=52,"razor"=54,"lobster"=57)
+
 ### Stochasticity
 set.seed(1234)
 nits=500
@@ -32,7 +37,10 @@ save(uDev, file=file.path(dirRes,"uDev.RData"))
 
 ### grid for control
 set.seed(123456)
-control=FLPars("1"=rbind(FLPar(k1   =rep(0.5, nits)),
+control=FLPars("0"=rbind(FLPar(k1   =rep(0.0, nits)),
+                         FLPar(k2   =rep(0.0, nits)),
+                         FLPar(gamma=rep(1,   nits))),
+               "1"=rbind(FLPar(k1   =rep(0.5, nits)),
                          FLPar(k2   =rep(0.5, nits)),
                          FLPar(gamma=rep(1,   nits))),
                "2"=rbind(FLPar(k1   =rep(0.5, nits)),
@@ -47,9 +55,9 @@ control=FLPars("1"=rbind(FLPar(k1   =rep(0.5, nits)),
 
 save(control,file=file.path(dirRes,"gCtrl.RData"))
 
-mseStart=c("brill"=54,"turbot"=54,"ray"=60,"pollack"=56,"sprat"=52,"razor"=54,"lobster"=57)
+
 scen=expand.grid(spp    =c("brill",   "turbot",   "ray",   "pollack",   "sprat",   "razor",   "lobster"),
-                 control=1:4,
+                 iCtrl=1:5,
                  stringsAsFactors=FALSE)
 
 fn<-function(iSpp,iCtrl){
@@ -57,13 +65,14 @@ fn<-function(iSpp,iCtrl){
   load(file.path(dirDat,paste(iSpp,".RData",sep="")))
 
   mse=mseSBTD(om,eq,
-            control=control,
+            control=control[[iCtrl]],
             srDev  =srDev,uDev=uDev,
             start  =mseStart[iSpp]+1,end=mseStart[iSpp]+46)
 
   save(mse,file=file.path(dirRes,paste("gridD-",iSpp,"-",iCtrl,".RData",sep="")))
   data.frame(spp=iSpp,control=iCtrl)}
 
+## missing
 gridD<-foreach(i=c(3,10,17,24),#seq(dim(scen)[1]), 
                 .combine=rbind,
                 .multicombine=TRUE,
@@ -71,4 +80,105 @@ gridD<-foreach(i=c(3,10,17,24),#seq(dim(scen)[1]),
                             "FLasher","FLBRP","FLife")) %dopar%{
                             
         with(scen[i,],fn(spp,control))}
+
+
+## Ray with 10 year index
+scen=expand.grid(spp =c("brill",   "turbot",   "ray",   "pollack",   "sprat",   "razor",   "lobster")[3],
+                 iCtrl=1:5,
+                 stringsAsFactors=FALSE)
+
+fn<-function(iSpp,iCtrl){
+  
+  load(file.path(dirDat,paste(iSpp,".RData",sep="")))
+  
+  mse=mseSBTD(om,eq,
+              control=control[[iCtrl]],
+              srDev  =srDev,uDev=uDev,
+              start  =mseStart[iSpp]+1,end=mseStart[iSpp]+46,
+              nyrs=10)
+  
+  save(mse,file=file.path(dirRes,paste("gridD-",iSpp,"-10yr-",iCtrl,".RData",sep="")))
+  data.frame(spp=iSpp,control=iCtrl)}
+
+gridD<-foreach(i=seq(dim(scen)[1]), 
+               .combine=rbind,
+               .multicombine=TRUE,
+               .packages=c("plyr","dplyr","reshape","ggplot2","FLCore","ggplotFL",
+                           "FLasher","FLBRP","FLife")) %dopar%{
+                             
+                             with(scen[i,],fn(spp,iCtrl))}
+
+## Sprat with recruit index
+scen=expand.grid(spp  =c("brill",   "turbot",   "ray",   "pollack",   "sprat",   "razor",   "lobster")[5],
+                 iCtrl=1:5,
+                 stringsAsFactors=FALSE)
+
+fn<-function(iSpp,iCtrl){
+  
+  load(file.path(dirDat,paste(iSpp,".RData",sep="")))
+  
+  mse=mseSBTD(om,eq,
+              control=control[[iCtrl]],
+              srDev  =srDev,uDev=uDev,
+              start  =mseStart[iSpp]+1,end=mseStart[iSpp]+46,
+              nyrs=5,
+              cpueFn =function(x) apply(stock.n(x)%*%(1-mat(x))%*%stock.wt(x),c(2,6),sum))
+  
+  save(mse,file=file.path(dirRes,paste("gridD-",iSpp,"-uJuv-",iCtrl,".RData",sep="")))
+  data.frame(spp=iSpp,control=iCtrl)}
+
+gridD<-foreach(i=seq(dim(scen)[1]), 
+               .combine=rbind,
+               .multicombine=TRUE,
+               .packages=c("plyr","dplyr","reshape","ggplot2","FLCore","ggplotFL",
+                           "FLasher","FLBRP","FLife")) %dopar%{
+                             
+                             with(scen[i,],fn(spp,iCtrl))}
+
+
+load("/home/laurence/Desktop/Dropbox/mydasOMs/results/gridD-sprat-uJuv-1.RData")
+sprat=FLStocks("k1=0; k2=0"=mse)
+load("/home/laurence/Desktop/Dropbox/mydasOMs/results/gridD-sprat-uJuv-2.RData")
+sprat["k1=0.5; k2=0.5"]=mse
+load("/home/laurence/Desktop/Dropbox/mydasOMs/results/gridD-sprat-uJuv-3.RData")
+sprat["k1=0.5; k2=1.0"]=mse
+load("/home/laurence/Desktop/Dropbox/mydasOMs/results/gridD-sprat-uJuv-4.RData")
+sprat["k1=1.0; k2=0.5"]=mse
+load("/home/laurence/Desktop/Dropbox/mydasOMs/results/gridD-sprat-uJuv-5.RData")
+sprat["k1=1.0; k2=1.0"]=mse
+sprat=FLStocks(llply(sprat,window,end=100))
+plot(sprat)
+
+load("/home/laurence/Desktop/Dropbox/mydasOMs/results/gridD-ray-10yr-1.RData")
+ray=FLStocks("k1=0; k2=0"=mse)
+load("/home/laurence/Desktop/Dropbox/mydasOMs/results/gridD-ray-10yr-2.RData")
+ray["k1=0.5; k2=0.5"]=mse
+load("/home/laurence/Desktop/Dropbox/mydasOMs/results/gridD-ray-10yr-3.RData")
+ray["k1=0.5; k2=1.0"]=mse
+load("/home/laurence/Desktop/Dropbox/mydasOMs/results/gridD-ray-10yr-4.RData")
+ray["k1=1.0; k2=0.5"]=mse
+load("/home/laurence/Desktop/Dropbox/mydasOMs/results/gridD-ray-10yr-5.RData")
+ray["k1=1.0; k2=1.0"]=mse
+ray=FLStocks(llply(ray,window,end=104))
+plot(ray)
+
+fn<-function(om){
+  as.data.frame(FLQuants(om, 
+                         "ssb" = function(x) ssb(x)%/%refpts( eq)["msy","ssb"], 
+                         "f" =   function(x) fbar(x)%/%refpts(eq)["msy","harvest"], 
+                         "rec" = function(x) rec(x)%/%refpts( eq)["msy","rec"], 
+                         "catch"=function(x) landings(x)%/%refpts(eq)["msy","yield"]))}
+
+load(file.path(dirDat,"sprat.RData"))
+sprat2=cbind(spp="sprat",ldply(sprat,fn))
+sprat2=subset(sprat2,year>mseStart["sprat"])
+sprat2$year=sprat2$year-mseStart["sprat"]
+
+load(file.path(dirDat,"ray.RData"))
+ray2=cbind(spp="ray",ldply(ray,fn))
+ray2=subset(ray2,year>mseStart["ray"])
+ray2$year=ray2$year-mseStart["ray"]
+
+smry2=rbind(sprat2,ray2)
+save(smry2,file=file.path(dirRes,"smry2.RData"))
 
--- tasks/task5/R/mse-emp-process.R
+++ tasks/task5/R/mse-emp-process.R
@@ -2,25 +2,47 @@
 
 library(plyr)
 library(dplyr)
+library(reshape)
 library(mydas)
+library(FLCore)
+library(ggplotFL)
+library(FLBRP)
+library(FLife)
 
 dirMy="/home/laurence/Desktop/Dropbox/mydasOMs"
 dirDat=file.path(dirMy, "data")
 dirRes=file.path(dirDat,"results")
 
-parD=NULL
-for (i in seq(dim(scen)[1])){
-  #drop_download(path=paste(file.path("mydasOMs",scen[i,"spp"]),".RData",sep=""),overwrite=T)
-  load(file.path(dirRes,paste(scen[i,"spp"],".RData",sep="")))
-  parD=rbind(parD,cbind(spp=scen[i,"spp"],model.frame(prior),model.frame(controlD)[,-4]))
-}
+source('~/Desktop/sea++/mydas/pkg/R/omOut.R')
 
-empD$spp =ac(empD$spp)
-empD     =subset(empD,year>=mseStart[spp]&year<=mseStart[spp]+45)
-empD$year=empD$year-mseStart[empD$spp]
+mseStart=c("brill"=54,"turbot"=54,"ray"=60,"pollack"=56,"sprat"=52,"razor"=54,"lobster"=57)
 
-empd_pm=ddply(empD,.(spp,iter), smryStat)
+fl=c("/home/laurence/Desktop/Dropbox/mydasOMs/results/ranD-y10-ray-1.RData",
+     "/home/laurence/Desktop/Dropbox/mydasOMs/results/ranD-y10-ray-2.RData",
+     "/home/laurence/Desktop/Dropbox/mydasOMs/results/ranD-y10-ray-3.RData",
+     "/home/laurence/Desktop/Dropbox/mydasOMs/results/ranD-y10-ray-4.RData",
+     "/home/laurence/Desktop/Dropbox/mydasOMs/results/ranD-y10-ray-5.RData",
+     "/home/laurence/Desktop/Dropbox/mydasOMs/results/ranD-y10-ray-6.RData",
+     "/home/laurence/Desktop/Dropbox/mydasOMs/results/ranD-y10-ray-7.RData",
+     "/home/laurence/Desktop/Dropbox/mydasOMs/results/ranD-y10-ray-8.RData",
+     "/home/laurence/Desktop/Dropbox/mydasOMs/results/ranD-y10-ray-9.RData",
+     "/home/laurence/Desktop/Dropbox/mydasOMs/results/ranD-y10-ray-10.RData",
+     "/home/laurence/Desktop/Dropbox/mydasOMs/results/ranD-y10-ray-11.RData",
+     "/home/laurence/Desktop/Dropbox/mydasOMs/results/ranD-y10-ray-12.RData")
 
-empd_pm=merge(transform(empd_pm,iter=as.numeric(ac(iter)),spp=ac(spp)),
-              transform(parD,   iter=as.numeric(ac(iter)),spp=ac(spp)),by=c("spp","iter"))
-save(empD,empd_pm,controlD,file=file.path(dirRes,"empd-results.RData"))
+load("/home/laurence/Desktop/Dropbox/mydasOMs/data/dCtrl.RData")
+
+empD=NULL
+for (i in c(1:4,6:7,9:10,12)){
+  load(fl[i])
+  empD=rbind(empD,cbind(iCtrl=i,omSmry(mse,eq,lh),model.frame(control[[i]])))}
+
+empD     =subset(empD,year>=mseStart["sprat"]&year<=mseStart["sprat"]+45)[,-26]
+empD$year=empD$year-mseStart["sprat"]
+empD=transform(empD,iter=as.numeric(ac(iter))+(iCtrl-1)*500)
+
+empd_pm=ddply(empD,.(iter,k1,k2), smryStat)
+
+
+
+
