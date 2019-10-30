37941b9ebac82f172419f73dff44d5c4e930c0fb
lauriekell <laurie@kell.es>
2019-10-30 13:58:50 +0000

    tidy up for final

--- R/shiny/albn/global.R
+++
@@ -1,7 +0,0 @@
-load("data/shiny.RData")
-names(priors)[1]="OM"
-iters=unique(mseIts$iter)
-
-library(plyr)
-library(dplyr)
-library(ggplot2)
--- R/xsaTest.R
+++
@@ -1,7 +0,0 @@
-xsaTest<-function(om,pg=10,ctrl=FLXSA.control()){
-  
-  stk=setPlusGroup(om,pg)
-  idx=FLIndex(index=stock.n(stk))
-  range(idx)[c("plusgroup","startf","endf")]=c(pg,0.01,.1)
-  
-  stk+FLXSA(stk,idx,control=ctrl,diag.flag=FALSE)}
